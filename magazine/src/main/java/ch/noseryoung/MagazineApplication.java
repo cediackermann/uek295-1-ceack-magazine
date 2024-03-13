c1752138c4884fbcc43d17dd2cda893ad2f10ef2
Cédric Ackermann <ackermanncedi@gmail.com>
2024-03-13 08:08:47 +0100

    Added Main Class

--- magazine/src/main/java/ch/noseryoung/MagazineApplication.java
+++ magazine/src/main/java/ch/noseryoung/MagazineApplication.java
@@ -1,4 +1,4 @@
-package ch.noseryoung.domain.magazine;
+package ch.noseryoung;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
